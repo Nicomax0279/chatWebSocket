ed96031ddd02f93f8f7864cb26cde1721fc57775
Nicomax0279 <92488330+Nicomax0279@users.noreply.github.com>
2022-10-15 16:03:17 -0300

    Initial commit

---
+++ src/public/index.js
@@ -0,0 +1,42 @@
+
+
+ 
+
+const socketCliente = io();
+let user ;
+Swal.fire({
+    title: "hola usuario",
+    text : "bienvenido, ingrese su usuario",
+    input: "text",
+    allowOutsideClick : false
+}).then( res=> user= res.value);
+
+socketCliente.on("menssageFromServer",(data)=>{
+    
+});
+const MenCon = document.getElementById("MensaggesContainer")
+const campo = document.getElementById("menssageField");
+
+campo.addEventListener("keydown",(evt)=>{
+    //console.log(evt.key)
+    if(evt.key === "Enter"){
+    socketCliente.emit("message",{menssage : campo.value, username : user})
+    }
+})
+socketCliente.on("menssages",(data)=>{
+let elementos = "";
+console.log(data)
+data.forEach(i => {
+    elementos += `<p><strong>${i.username}</strong>:${i.menssage}</p>`
+
+    
+});
+MenCon.innerHTML = elementos
+})  
+socketCliente.on("newUser",(data)=>{
+    Swal.fire({
+        text : "nuevo usuario conectado",
+        toast : true 
+
+    })
+})
---
+++ src/server.js
@@ -0,0 +1,34 @@
+
+// import express from 'express'
+// import {Server} from 'socket.io'
+const express = require('express')
+const {Server} = require('socket.io')
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+"/public"));
+
+const server = app.listen(PORT,()=>console.log(`listening on PORT: ${PORT}`))
+//io: wedsocket Servers
+
+const io = new Server(server);
+const historicoMensajaes = [];
+io.on("connection",(socket)=>{
+    socket.emit("menssageFromServer", "se ha conectao existosamente")
+    console.log(socket.id)
+    socket.broadcast.emit("newUser",socket.id)
+    socket.emit("menssages",historicoMensajaes)
+
+    socket.on("message",(data)=>{
+        console.log(data)
+        historicoMensajaes.push(data)
+        io.sockets.emit("menssages",historicoMensajaes)
+    })
+
+    
+
+})
